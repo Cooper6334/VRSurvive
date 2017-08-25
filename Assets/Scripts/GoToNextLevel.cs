ed7ec0cade06133cd8e40c595d771410c7fdcc85
Denny <denny@denny.com>
2017-08-25 14:52:04 +0800

    Create LevelManager

---
+++ Assets/Scripts/GoToNextLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToNextLevel : MonoBehaviour
+{
+	void Start ()
+	{
+		LevelManager.Instance.GoToNextLevel ();
+	}
+}
