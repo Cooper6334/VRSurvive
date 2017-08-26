46ea329b9dd2c993d469056fb10ea2cf818fefbb
Denny <denny@denny.com>
2017-08-26 13:16:45 +0800

    Add fishes

---
+++ Assets/Objects/Fish/Scripts/RoastFishDisplayer.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoastFishDisplayer : MonoBehaviour
+{
+	[SerializeField]
+	private Renderer renderer;
+	private Material mat;
+
+	private float LeftRawWeight
+	{
+		get {
+			return mat.GetFloat ("_LeftRawWeight");
+		}
+
+		set {
+			mat.SetFloat ("_LeftRawWeight", value);
+		}
+	}
+
+	private float LeftRipeWeight
+	{
+		get {
+			return mat.GetFloat ("_LeftRipeWeight");
+		}
+
+		set {
+			mat.SetFloat ("_LeftRipeWeight", value);
+		}
+	}
+
+	private float LeftBurnedWeight
+	{
+		get {
+			return mat.GetFloat ("_LeftBurnedWeight");
+		}
+
+		set {
+			mat.SetFloat ("_LeftBurnedWeight", value);
+		}
+	}
+
+	private float RightRawWeight
+	{
+		get {
+			return mat.GetFloat ("_RightRawWeight");
+		}
+
+		set {
+			mat.SetFloat ("_RightRawWeight", value);
+		}
+	}
+
+	private float RightRipeWeight
+	{
+		get {
+			return mat.GetFloat ("_RightRipeWeight");
+		}
+
+		set {
+			mat.SetFloat ("_RightRipeWeight", value);
+		}
+	}
+
+	private float RightBurnedWeight
+	{
+		get {
+			return mat.GetFloat ("_RightBurnedWeight");
+		}
+
+		set {
+			mat.SetFloat ("_RightBurnedWeight", value);
+		}
+	}
+
+	public void SetLeftRaw()
+	{
+		LeftRawWeight = 1.0f;
+		LeftRipeWeight = 0.0f;
+		LeftBurnedWeight = 0.0f;
+	}
+
+	public void SetLeftRipe()
+	{
+		LeftRawWeight = 0.0f;
+		LeftRipeWeight = 1.0f;
+		LeftBurnedWeight = 0.0f;
+	}
+
+	public void SetLeftBurned()
+	{
+		LeftRawWeight = 0.0f;
+		LeftRipeWeight = 0.0f;
+		LeftBurnedWeight = 1.0f;
+	}
+
+	public void SetRightRaw()
+	{
+		RightRawWeight = 1.0f;
+		RightRipeWeight = 0.0f;
+		RightBurnedWeight = 0.0f;
+	}
+
+	public void SetRightRipe()
+	{
+		RightRawWeight = 0.0f;
+		RightRipeWeight = 1.0f;
+		RightBurnedWeight = 0.0f;
+	}
+
+	public void SetRightBurned()
+	{
+		RightRawWeight = 0.0f;
+		RightRipeWeight = 0.0f;
+		RightBurnedWeight = 1.0f;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		mat = renderer.material;
+		LeftRawWeight = 1.0f;
+		LeftRipeWeight = 0.0f;
+		LeftBurnedWeight = 0.0f;
+		RightRawWeight = 1.0f;
+		RightRipeWeight = 0.0f;
+		RightBurnedWeight = 0.0f;
+	}
+
+	/*void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.A)) {
+			SetLeftRaw ();
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			SetLeftRipe ();
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			SetLeftBurned ();
+		}
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			SetRightRaw ();
+		}
+		if (Input.GetKeyDown (KeyCode.X)) {
+			SetRightRipe ();
+		}
+		if (Input.GetKeyDown (KeyCode.C)) {
+			SetRightBurned ();
+		}
+	}*/
+}
---
+++ Assets/Objects/Fish/Shader/FishShader.shader
@@ -0,0 +1,82 @@
+﻿Shader "Custom/FishShader"
+{
+	Properties
+	{
+		_MainTex ("Raw Texture", 2D) = "white" {}
+		_RipeTex ("Ripe Texture", 2D) = "white" {}
+		_BurnedTex ("Burned Texture", 2D) = "white" {}
+
+		_LeftRawWeight ("Left Raw Weight", Range(0,1)) = 1
+		_LeftRipeWeight ("Left Ripe Weight", Range(0,1)) = 1
+		_LeftBurnedWeight ("Left Burned Weight", Range(0,1)) = 1
+		_RightRawWeight ("Right Raw Weight", Range(0,1)) = 1
+		_RightRipeWeight ("Right Ripe Weight", Range(0,1)) = 1
+		_RightBurnedWeight ("Right Burned Weight", Range(0,1)) = 1
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+				float2 leftRightWeight : TEXCOORD1;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			sampler2D _RipeTex;
+			float4 _RipeTex_ST;
+			sampler2D _BurnedTex;
+			float4 _BurnedTex_ST;
+
+			float _LeftRawWeight;
+			float _LeftRipeWeight;
+			float _LeftBurnedWeight;
+			float _RightRawWeight;
+			float _RightRipeWeight;
+			float _RightBurnedWeight;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				float rightWeight = (max(-0.01, min(0.01, v.vertex.z)) + 0.01) / 0.02;
+				o.leftRightWeight = float2(1 - rightWeight, rightWeight);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = (tex2D(_MainTex, i.uv) * _LeftRawWeight + tex2D(_RipeTex, i.uv) * _LeftRipeWeight + tex2D(_BurnedTex, i.uv) * _LeftBurnedWeight) * i.leftRightWeight.x
+					+ (tex2D(_MainTex, i.uv) * _RightRawWeight + tex2D(_RipeTex, i.uv) * _RightRipeWeight + tex2D(_BurnedTex, i.uv) * _RightBurnedWeight) * i.leftRightWeight.y;
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
