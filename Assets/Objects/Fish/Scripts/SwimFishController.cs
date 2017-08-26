5ccd91ed9938c68082e834b8c22dba6692196308
Denny <denny@denny.com>
2017-08-26 14:59:06 +0800

    Add swimming fishes.

---
+++ Assets/Objects/Fish/Scripts/SwimFishController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwimFishController : MonoBehaviour
+{
+	public Vector3 speed;
+	private Vector3 originPos;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<FishResetTrigger> () != null) {
+			transform.position = originPos;
+		}
+	}
+
+	void Start ()
+	{
+		originPos = transform.position;
+	}
+
+	void Update ()
+	{
+		transform.position += speed * Time.deltaTime;
+	}
+}
